4a608bfcb5c473dccb35378e6338b3ab5284708d
placidowang <placidow@gmail.com>
2020-02-24 14:56:13 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get '/coupons/', to: 'coupons#index'
+  get '/coupons/new', to: 'coupons#new', as: :new_coupon
+  post '/coupons/', to: 'coupons#create'
+  get '/coupons/:id', to: 'coupons#show', as: :coupon
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
